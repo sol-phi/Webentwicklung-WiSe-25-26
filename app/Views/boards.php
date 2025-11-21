0a3a480e9b43b4aee0947b4e78e4fc8825788763
masmir123 <117976753+masmir123@users.noreply.github.com>
2025-11-21 18:14:52 +0100

    mt-auto anstatt über main-class -> weniger Code

--- app/Views/boards.php
+++ app/Views/boards.php
@@ -1,5 +1,4 @@
-<!-- Die main-Klasse sorgt dafür, dass der Container nach unten langgezogen wird, sodass der Footer unten auf der Seite steht. -->
-<div class="main container mt-4">
+<div class="container mt-4">
     <!-- margin_top-4 -->
     <div class="card">
         <!-- font_size-4 -->
--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
     <body>
-        <!-- padding_y-3 -->
-        <footer class="bg-blue py-3 mt-4">
+        <!-- padding_y-3, dasselbe wie pt-3 pb-3 -->
+        <footer class="bg-blue py-3 mt-auto">
             <!-- Flex-Container, erstes Element links, und zweites Element rechts. Abstand automatisch zum Ausfüllen ermittelt-->
             <div class="container d-flex justify-content-between">
                 <div>
